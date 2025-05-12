9577ff71fb783ed16c11b908a778d28f81cf5674
Borys Ziukov <ziukoff1985@gmail.com>
2025-05-12 20:49:55 +0300

    Add fetchBrandsThunk to App.jsx with useEffect for initial data loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { fetchBrandsThunk } from "./redux/cars/operations.js";
+import { useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBrandsThunk());
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route path="/" element={<div>Home Page</div>} />
