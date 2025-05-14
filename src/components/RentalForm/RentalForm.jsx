f8592dfacd09cb3c438418ee8e353228b451aeb3
Borys Ziukov <ziukoff1985@gmail.com>
2025-05-14 11:36:45 +0300

    Add react-datepicker library, implement DatePicker in RentalForm

--- src/components/RentalForm/RentalForm.jsx
+++ src/components/RentalForm/RentalForm.jsx
@@ -1,21 +1,33 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import toast from "react-hot-toast";
-import styles from "./RentalForm.module.css";
+
 import rentalFormValidationSchema from "../../validationHelpers/rentalFormValidation.js";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import styles from "./RentalForm.module.css";
 
 const RentalForm = () => {
   const initialValues = {
     name: "",
     email: "",
-    bookingDate: "",
+    bookingDate: null,
     comment: "",
   };
 
   const handleSubmit = (values) => {
     toast.success(
-      `Rental request submitted: ${JSON.stringify(values, null, 2)}`,
+      `Rental request submitted: ${JSON.stringify(
+        {
+          ...values,
+          bookingDate: values.bookingDate
+            ? values.bookingDate.toISOString().split("T")[0]
+            : "",
+        },
+        null,
+        2
+      )}`,
       {
-        duration: 6000,
+        duration: 4000,
       }
     );
   };
@@ -29,38 +41,53 @@ const RentalForm = () => {
         validationSchema={rentalFormValidationSchema}
         onSubmit={handleSubmit}
       >
-        <Form className={styles.rentalForm}>
-          <Field
-            type="text"
-            name="name"
-            placeholder="Name*"
-            className={styles.input}
-          />
-          <ErrorMessage name="name" component="div" className={styles.error} />
-          <Field
-            type="email"
-            name="email"
-            placeholder="Email*"
-            className={styles.input}
-          />
-          <ErrorMessage name="email" component="div" className={styles.error} />
-          <Field type="date" name="bookingDate" className={styles.input} />
-          <ErrorMessage
-            name="bookingDate"
-            placeholder="Booking Date"
-            component="div"
-            className={styles.error}
-          />
-          <Field
-            as="textarea"
-            name="comment"
-            placeholder="Comment"
-            className={styles.textarea}
-          />
-          <button type="submit" className={styles.rentalButton}>
-            Send
-          </button>
-        </Form>
+        {({ setFieldValue, values }) => (
+          <Form className={styles.rentalForm}>
+            <Field
+              type="text"
+              name="name"
+              placeholder="Name*"
+              className={styles.input}
+            />
+            <ErrorMessage
+              name="name"
+              component="div"
+              className={styles.error}
+            />
+            <Field
+              type="email"
+              name="email"
+              placeholder="Email*"
+              className={styles.input}
+            />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className={styles.error}
+            />
+            <DatePicker
+              selected={values.bookingDate}
+              onChange={(date) => setFieldValue("bookingDate", date)}
+              placeholderText="Booking Date*"
+              className={styles.input}
+              dateFormat="MM/dd/yyyy"
+            />
+            <ErrorMessage
+              name="bookingDate"
+              component="div"
+              className={styles.error}
+            />
+            <Field
+              as="textarea"
+              name="comment"
+              placeholder="Comment"
+              className={styles.textarea}
+            />
+            <button type="submit" className={styles.rentalButton}>
+              Send
+            </button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
