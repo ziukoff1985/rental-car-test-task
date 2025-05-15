a97f9419e66d3c9975bb3b73f78b1cef434c2eba
Borys Ziukov <ziukoff1985@gmail.com>
2025-05-15 15:49:50 +0300

    Remove unused comments

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,109 +1,3 @@
-// import { useSelector, useDispatch } from 'react-redux';
-// import { selectBrands, selectFilters } from '../../redux/cars/selectors.js';
-// import { setFilters, clearFilters } from '../../redux/cars/slice.js';
-// import styles from './Filter.module.css';
-// import { fetchCarsThunk } from '../../redux/cars/operations.js';
-
-// const Filter = ({ onSearch }) => {
-//   const dispatch = useDispatch();
-//   const brands = useSelector(selectBrands);
-//   const filters = useSelector(selectFilters);
-
-//   const handleFilterChange = e => {
-//     const { name, value } = e.target;
-//     dispatch(setFilters({ [name]: value }));
-//   };
-
-//   const handleClearFilters = () => {
-//     dispatch(clearFilters());
-//     // Викликаємо fetchCarsThunk із порожніми фільтрами, щоб відобразити всі авто
-//     dispatch(
-//       fetchCarsThunk({
-//         page: 1,
-//         filters: { brand: '', rentalPrice: '', minMileage: '', maxMileage: '' },
-//       })
-//     );
-//   };
-
-//   return (
-//     <div className={styles.filter}>
-//       <label className={styles.label}>
-//         <span>Car brand</span>
-//         <div className={styles.selectWrapper}>
-//           <select
-//             name="brand"
-//             value={filters.brand}
-//             onChange={handleFilterChange}
-//             className={styles.select}
-//           >
-//             <option value="" disabled hidden>
-//               Choose a brand
-//             </option>
-//             {brands.map(brand => (
-//               <option key={brand} value={brand}>
-//                 {brand}
-//               </option>
-//             ))}
-//           </select>
-//         </div>
-//       </label>
-
-//       <label className={styles.label}>
-//         <span>Price/1 hour</span>
-//         <div className={styles.selectWrapper}>
-//           <select
-//             name="rentalPrice"
-//             value={filters.rentalPrice}
-//             onChange={handleFilterChange}
-//             className={styles.select}
-//           >
-//             <option value="" disabled hidden>
-//               Choose a price
-//             </option>
-//             {[30, 40, 50, 60, 70, 80].map(price => (
-//               <option key={price} value={price}>
-//                 {price}
-//               </option>
-//             ))}
-//           </select>
-//         </div>
-//       </label>
-
-//       <label className={styles.label}>
-//         <span>Car mileage / km</span>
-//         <div className={styles.mileage}>
-//           <input
-//             type="number"
-//             name="minMileage"
-//             value={filters.minMileage}
-//             onChange={handleFilterChange}
-//             placeholder="From"
-//             className={styles.inputFrom}
-//           />
-//           <input
-//             type="number"
-//             name="maxMileage"
-//             value={filters.maxMileage}
-//             onChange={handleFilterChange}
-//             placeholder="To"
-//             className={styles.inputTo}
-//           />
-//         </div>
-//       </label>
-
-//       <div className={styles.buttons}>
-//         <button onClick={onSearch} className={styles.searchButton}>
-//           Search
-//         </button>
-//         <button onClick={handleClearFilters} className={styles.clearButton}>
-//           Clear
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Filter;
 import { useSelector, useDispatch } from 'react-redux';
 import { selectBrands, selectFilters } from '../../redux/cars/selectors.js';
 import { setFilters, clearFilters } from '../../redux/cars/slice.js';
@@ -298,3 +192,110 @@ const Filter = ({ onSearch }) => {
 };
 
 export default Filter;
+
+// import { useSelector, useDispatch } from 'react-redux';
+// import { selectBrands, selectFilters } from '../../redux/cars/selectors.js';
+// import { setFilters, clearFilters } from '../../redux/cars/slice.js';
+// import styles from './Filter.module.css';
+// import { fetchCarsThunk } from '../../redux/cars/operations.js';
+
+// const Filter = ({ onSearch }) => {
+//   const dispatch = useDispatch();
+//   const brands = useSelector(selectBrands);
+//   const filters = useSelector(selectFilters);
+
+//   const handleFilterChange = e => {
+//     const { name, value } = e.target;
+//     dispatch(setFilters({ [name]: value }));
+//   };
+
+//   const handleClearFilters = () => {
+//     dispatch(clearFilters());
+//     // Викликаємо fetchCarsThunk із порожніми фільтрами, щоб відобразити всі авто
+//     dispatch(
+//       fetchCarsThunk({
+//         page: 1,
+//         filters: { brand: '', rentalPrice: '', minMileage: '', maxMileage: '' },
+//       })
+//     );
+//   };
+
+//   return (
+//     <div className={styles.filter}>
+//       <label className={styles.label}>
+//         <span>Car brand</span>
+//         <div className={styles.selectWrapper}>
+//           <select
+//             name="brand"
+//             value={filters.brand}
+//             onChange={handleFilterChange}
+//             className={styles.select}
+//           >
+//             <option value="" disabled hidden>
+//               Choose a brand
+//             </option>
+//             {brands.map(brand => (
+//               <option key={brand} value={brand}>
+//                 {brand}
+//               </option>
+//             ))}
+//           </select>
+//         </div>
+//       </label>
+
+//       <label className={styles.label}>
+//         <span>Price/1 hour</span>
+//         <div className={styles.selectWrapper}>
+//           <select
+//             name="rentalPrice"
+//             value={filters.rentalPrice}
+//             onChange={handleFilterChange}
+//             className={styles.select}
+//           >
+//             <option value="" disabled hidden>
+//               Choose a price
+//             </option>
+//             {[30, 40, 50, 60, 70, 80].map(price => (
+//               <option key={price} value={price}>
+//                 {price}
+//               </option>
+//             ))}
+//           </select>
+//         </div>
+//       </label>
+
+//       <label className={styles.label}>
+//         <span>Car mileage / km</span>
+//         <div className={styles.mileage}>
+//           <input
+//             type="number"
+//             name="minMileage"
+//             value={filters.minMileage}
+//             onChange={handleFilterChange}
+//             placeholder="From"
+//             className={styles.inputFrom}
+//           />
+//           <input
+//             type="number"
+//             name="maxMileage"
+//             value={filters.maxMileage}
+//             onChange={handleFilterChange}
+//             placeholder="To"
+//             className={styles.inputTo}
+//           />
+//         </div>
+//       </label>
+
+//       <div className={styles.buttons}>
+//         <button onClick={onSearch} className={styles.searchButton}>
+//           Search
+//         </button>
+//         <button onClick={handleClearFilters} className={styles.clearButton}>
+//           Clear
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Filter;
